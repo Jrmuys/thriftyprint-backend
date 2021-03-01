4608e696ced5211fc96c4c0bbce6671faf9f6296
Joel Muyskens <jrmuyskens@gmail.com>
2021-02-28 22:16:34 -0500

    Added password reset base

--- controller/reset.controller.js
+++ controller/reset.controller.js
@@ -1,16 +1,24 @@
+const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
+const PasswordReset = require("../models/password-reset.model")
+const User = require("../models/user.model")
 
 
+async function resetRequest(email) {
+    user = await User.findOne({ email: email }).catch((err) => {
+        throw new Error("Email not found");
+    })
+    result = await PasswordReset.create(new PasswordReset({
+        tokenHash: 
+    }))
+}
 
-function resetRequest(email) {
+async function checkToken(token) {
 
 }
 
-function checkToken(token) {
+async function resetPassword(pwd) {
 
 }
 
-function resetPassword(pwd) {
-
-}
-
-module.exports = { resetPassword, resetRequest }
+module.exports = { resetPassword, resetRequest, checkToken }
